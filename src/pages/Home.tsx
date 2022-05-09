caf5767d8ac6850fdd830aa796793e67fdb49038
Ahmad Khidir <khidirahmad05@gmail.com>
2022-05-09 11:00:32 +0100

    landing page (showcase)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import { ListView } from "../components/ListView";
+import { ListView } from "../components/List";
 import { AppBar } from "../features/AppBar";
 import { Header } from "../features/Header";
 import { Intro } from "../features/Intro";
 import { LearningCentre } from "../features/LearningCentre";
+import { Showcase } from "../features/Showcase";
 
 export function Home(props:any) {
     return (
@@ -11,6 +12,7 @@ export function Home(props:any) {
                 <Header />
                 <Intro />
                 <LearningCentre />
+                <Showcase />
             </ListView>
         </main>
     )
