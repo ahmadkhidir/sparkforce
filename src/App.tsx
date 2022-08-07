3977e12964f5e5e574547a3dd7bfb5c4d7b7358d
Ahmad Khidir <khidirahmad05@gmail.com>
2022-08-07 12:09:52 +0100

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { Home } from "./pages/home/Home";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { WaitList } from "./pages/WaitList";
+import WaitList from "./pages/waitlist/WaitList";
 
 function App() {
   return (
